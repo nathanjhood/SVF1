6c08fbee3e291a4d77558c0a3e426a0a909bed4b
Nathan Hood <nathanjhood@googlemail.com>
2022-03-10 06:26:47 +0100

    Added VST3-style extendable Buss arcitecture

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -139,6 +139,9 @@ bool SVF1AudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) con
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
+    if (layouts.getMainInputChannelSet()  == juce::AudioChannelSet::disabled()
+     || layouts.getMainOutputChannelSet() == juce::AudioChannelSet::disabled())
+
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
@@ -149,7 +152,7 @@ bool SVF1AudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) con
         return false;
    #endif
 
-    return true;
+    return layouts.getMainInputChannelSet() == layouts.getMainOutputChannelSet();
   #endif
 }
 #endif
